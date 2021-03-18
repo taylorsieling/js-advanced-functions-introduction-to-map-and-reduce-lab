c4a5b77c38c96dfb804dddd685945a3bf4579f7f
taylorsieling <taylorasieling@gmail.com>
2021-03-18 15:10:03 -0500

    commit tree 7f9008e7f61658136e0794af4a0495defb0a7445

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
-// Your code here
+// map-like functions
+// remember to return a new array
+
+function mapToNegativize(sourceArray) {
+    let newArray = []
+    sourceArray.forEach(element => {
+        newArray.push(element * -1);
+    });
+    return newArray
+}
+
+function mapToNoChange(sourceArray) {
+    let newArray = []
+    sourceArray.forEach(element => {
+        newArray.push(element);
+    });
+    return newArray
+}
+
+function mapToDouble(sourceArray) {
+    let newArray = []
+    sourceArray.forEach(element => {
+        newArray.push(element *2);
+    });
+    return newArray
+}
+
+function mapToSquare(sourceArray) {
+    let newArray = []
+    sourceArray.forEach(element => {
+        newArray.push(element * element)
+    });
+    return newArray
+}
+
+// reduce-like functions
+// remember to return a value
+
+function reduceToTotal(sourceArray, startingPoint) {
+    let total = startingPoint != undefined ? startingPoint : 0;
+    sourceArray.forEach(element => {
+        total = total + element;
+    });
+    return total
+}
+
+function reduceToAllTrue(sourceArray) {
+    let value = true
+    sourceArray.forEach(element => {
+        if (!element) {value = false}
+    })
+    return value
+}
+function reduceToAnyTrue(sourceArray) {
+    let value = false
+    sourceArray.forEach(element => {
+        if (!!element) {value = true}
+    })
+    return value
+}
